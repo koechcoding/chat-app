7d3c13b4955a3c49f99e60acef23891ba9a0b56f
koechcoding <koechkelvin2001@gmail.com>
2022-11-04 12:24:29 +0300

    Add cloud functions and real time data

---
+++ src/redux/actions/notificationActions.js
@@ -0,0 +1,16 @@
+//Post message constants
+export const POST_MESSAGE = 'POST_MESSAGE';
+export const POST_MESSAGE_SUCCESS = 'POST_MESSAGE_SUCCESS';
+export const POST_MESSAGE_FAILURE = 'POST_MESSAGE_FAILURE';
+
+
+//Chat constants
+export const GET_CHATS = 'GET_CHATS';
+export const GET_CHATS_SUCCESS = 'GET_CHATS_SUCCESS';
+export const GET_CHATS_FAILURE = 'GET_CHATS_FAILURE';
+
+
+//Notifications constants
+export const GET_NOTIFICATIONS = 'GET_NOTIFICATIONS';
+export const GET_NOTIFICATIONS_SUCCESS = 'GET_NOTIFICATIONS_SUCCESS';
+export const GET_NOTIFICATIONS_FAILURE = 'GET_NOTIFICATIONS_FAILURE';
