4b948c7ace836f4e3ab611587240e0590dbce6f1
koechcoding <koechkelvin2001@gmail.com>
2022-11-04 11:53:54 +0300

    Add cloud functions and real time data

---
+++ src/containers/chats/Chats.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './chats.scss';
+import Input from '../../components/chat/Input';
+import { connect } from 'react-redux';
+import { getChats } from '../../redux/actions/chatActions';
+import Notifications from './Notifications';
+
+class Chats extends Component{
+    componentDidMount(){
+        const { getChats} = this.props;
+        getChats();
+    }
+
+    render(){
+        const { chats } = this.props;
+        return(
+            <div className='main-container container'>
+                <div className='panel panel-container'>
+                    <h1 className='panel-heading panel-container_heading'>ChatRoom</h1>
+                    <div className='panel-block panel-message'>
+                        {
+                            chats && chats.map(chat=>{
+                                return(
+                                    <div key={chat.id} className="panel-messages__message">{chat.message}</div>
+                                );
+                            })
+                        }
+                    </div>
+                    <input/>
+                </div>
+                <div className='notifications-container'>
+                     <Notifications />
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+chats: state.chats.chats
+})
+
+const mapDispatchToProps = {
+getChats
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chats);
