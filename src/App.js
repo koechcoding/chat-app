4a5168835c4fae3a2f3d27b7a5e629dcf9e76ed6
koechcoding <koechkelvin2001@gmail.com>
2022-11-07 10:29:30 +0300

    Add cloud functions and real time data

--- src/App.js
+++ src/App.js
@@ -3,3 +3,22 @@ import Navbar from './components/layout/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import Chats from './containers/chats/Chats';
 import Signin from './containers/auth/Signin';
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <div>
+          <Navbar/>
+        </div>
+        <Switch>
+          <Route exact path='/' component={Chats}></Route>
+          <Route path="/signin" component={Signin}></Route>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
