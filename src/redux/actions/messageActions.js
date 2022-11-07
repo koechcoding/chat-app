629ed7b639358bae12d43179846c371c9151e8c8
koechcoding <koechkelvin2001@gmail.com>
2022-11-07 06:49:46 +0300

    Add cloud functions and real time data

--- src/redux/actions/messageActions.js
+++ src/redux/actions/messageActions.js
@@ -4,3 +4,26 @@ import {
     POST_MESSAGE_FAILURE
   } from '../actions/actionTypes';
   import { fb } from '../../config/fbConfig';
+  
+  export const postMessage = message => dispatch => {
+    console.log('Message', message);
+    dispatch({ type: POST_MESSAGE });
+    fb.firestore()
+      .collection('chatroom')
+      .add(message)
+      .then(data => {
+        data
+          .get()
+          .then(doc =>
+            dispatch({ type: POST_MESSAGE_SUCCESS, data: doc.data() })
+          )
+          .catch(error => {
+            console.log('Message posting failed', error);
+            dispatch({ type: POST_MESSAGE_FAILURE, error });
+          });
+      })
+      .catch(error => {
+        console.log('Message posting failed', error);
+        dispatch({ type: POST_MESSAGE_FAILURE, error });
+      });
+  };
